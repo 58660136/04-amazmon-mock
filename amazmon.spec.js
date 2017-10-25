1f907f7a0c701da54d81e9122d6bdf7a371c35d9
Jame <58660136@go.buu.ac.th>
2017-10-25 14:17:42 +0700

    function booksearching

--- amazmon.spec.js
+++ amazmon.spec.js
@@ -9,3 +9,49 @@ test('Mock function should be return 1',() => {
     expect(testApp(1)).toBe(1)
     expect(testApp).toBeCalledWith(1)
 })
+
+function AmazmonAuthen(isbn) {
+    this.authService = isbn
+
+    this.Search = (isbn) => {
+        var book = this.authService(isbn)
+        return {
+            /*
+            bookname: book.bookname,
+            image: book.image,
+            isbn: book.isbn
+            */
+            bookname: 'PHP Mysql',
+            image: 'sql.png',
+            isbn: 'Bnk1536'
+        }
+    }
+}
+
+test('AmazmonAuthen', ()=> {
+    //Arrange
+    const mockAmazmonAuthen = jest.fn()
+        .mockReturnValue({
+            bookname: 'PHP Mysql',
+            image: 'sql.png',
+            isbn: 'Bnk1536'
+        })
+    
+    //Act
+    var app = new AmazmonAuthen(mockAmazmonAuthen)
+    var isbn = 'Bnk1536'
+    var book = app.Search(isbn)
+
+    //Assert
+    expect(mockAmazmonAuthen).toHaveBeenCalled()
+    expect(mockAmazmonAuthen).toHaveBeenCalledWith(isbn)
+    expect(book).toHaveProperty('bookname')
+    expect(book).toHaveProperty('image')
+    expect(book).toHaveProperty('isbn')
+    expect(book.bookname).toBe('PHP Mysql')
+    expect(book.image).toBe('sql.png')
+    expect(book.isbn).toHaveLength(7)
+    expect(book).toHaveProperty('bookname')
+    expect(book).toHaveProperty('image')
+    expect(book).toHaveProperty('isbn')
+})
